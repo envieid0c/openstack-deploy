3e892cec1dea3ce7aa05e20b55e60eb98d1010d4
Vladimir Eremin <yottatsa@yandex-team.ru>
2014-10-23 18:42:12 +0400

    mysql

--- 01-install-percona.sh
+++ 01-install-percona.sh
@@ -1,7 +1,9 @@
 #!/bin/sh -x
 
 apt-add-repository 'deb http://repo.percona.com/apt trusty main'
-apt-get install percona-xtradb-cluster-56 -y --force-yes -qq
+apt-get update
+apt-get install envsubst percona-xtradb-cluster-56 -y --force-yes -qq
 
 export ip=$(hostname -i)
+mkdir -p /etc/mysql/conf.d
 evnsubst < configs/galera-cluster.cnf > /etc/mysql/conf.d/galera-cluster.cnf
