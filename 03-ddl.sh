8723527343a27e210c053c8e1e48bd4f2afff827
Vladimir Eremin <yottatsa@yandex-team.ru>
2014-10-24 17:01:03 +0400

    ddl

--- 03-ddl.sh
+++ 03-ddl.sh
@@ -4,13 +4,16 @@ mysql -e "CREATE USER 'sstuser'@'localhost' IDENTIFIED BY 's3cretPass';"
 
 mysql -e "GRANT RELOAD, LOCK TABLES, REPLICATION CLIENT ON *.* TO 'sstuser'@'localhost';"
 
-mysql -e "CREATE USER 'sstuser'@'%' IDENTIFIED BY 's3cretPass';"
+mysql -e "GRANT USAGE ON *.* TO 'sstuser'@'host1' IDENTIFIED BY 's3cretPass';"
+mysql -e "GRANT ALL ON *.* to 'openstack'@'host1' IDENTIFIED BY 's3cretPass';"
 
-mysql -e "GRANT USAGE ON *.* TO 'sstuser'@'%';"
+mysql -e "GRANT USAGE ON *.* TO 'sstuser'@'host2' IDENTIFIED BY 's3cretPass';"
+mysql -e "GRANT ALL ON *.* to 'openstack'@'host2' IDENTIFIED BY 's3cretPass';"
 
-mysql -e "CREATE USER 'openstack'@'%' IDENTIFIED BY 's3cretPass';"
+mysql -e "GRANT USAGE ON *.* TO 'sstuser'@'host3' IDENTIFIED BY 's3cretPass';"
+mysql -e "GRANT ALL ON *.* to 'openstack'@'host3' IDENTIFIED BY 's3cretPass';"
 
-mysql -e "GRANT ALL ON *.* to 'openstack'@'%';"
+mysql -e "GRANT ALL ON *.* to 'openstack'@'localhost' IDENTIFIED BY 's3cretPass';"
 
 mysql -e "FLUSH PRIVILEGES;"
 
@@ -18,7 +21,7 @@ mysql -e "CREATE DATABASE IF NOT EXISTS nova;"
 
 mysql -e "CREATE DATABASE IF NOT EXISTS neutron;"
 
-mysql -e "CREATE DATABASE IF NOT EXISTS glance;"
+mysql -e "CREATE DATABASE IF NOT EXISTS glance CHARSET=utf8;"
 
 mysql -e "CREATE DATABASE IF NOT EXISTS keystone;"
 
